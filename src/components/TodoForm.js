7bcd1733c3498dfda7ecda78eee476ae6cd92f16
Jerry Udochu <udochujerry@gmail.com>
2024-06-05 13:36:19 +0100

    Adding the build directory

---
+++ src/components/TodoForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Check if value is empty, it doesn't add a task
+    if (value.trim() === "") {
+      return alert("No task added! Please enter a task!!!");
+    }
+
+    addTodo(value);
+
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="Enter a task..."
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
